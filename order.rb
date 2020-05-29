6c5ec19450204e1a7aab655a10c4dfe34be3cb72
vladveterok <vl.berezhnoi@gmail.com>
2020-05-29 19:54:56 +0300

    Add add_book and add_reader method to the Order class:
    add_book for adding a book to an order
    add_reader for adding a reader to an order

--- order.rb
+++ order.rb
@@ -1,31 +1,47 @@
 # frozen_string_literal: true
 
+require_relative 'book'
+require_relative 'reader'
+
 # Orders are made by readers who want to pick up a book
 class Order
   def initialize(book = 'test', reader = 'test', date = Time.new.strftime('%d/%m/%Y'))
     # Some code here
     @book = add_book(book)
-    @reader = add_Reader(reader)
+    @reader = add_reader(reader)
     @date = date
   end
 
   private
 
   def add_book(book)
-    # Some code here
+    Book.all_books.find { |b| b.title == book }
   end
 
   def add_reader(reader)
-    # Some code here
+    Reader.all_readers.find { |r| r.name == reader }
   end
 
   def to_s
-    "type: #{self.class.name}, " \
-    "book: #{@book}, " \
-    "reader: #{@reader}, " \
-    "date: #{@date}"
+    "type: #{self.class.name},\n
+    book: #{@book},\n
+    reader: #{@reader},\n
+    date: #{@date}"
   end
 end
 
-order1 = Order.new
+# Books
+book1 = Book.new("Book 1", "Foo")
+
+# Readers
+reader1 = Reader.new(
+  name: "Alex",
+  email: "alex@gmail.com",
+  city: "Kyiv",
+  street: "Shoto st",
+  house: "kek"
+)
+
+# Orders 
+order1 = Order.new("Book 1", "Alex")
 puts order1
