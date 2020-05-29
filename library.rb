de5bdf8536294e5550f571e53d1d3c344df1a7f3
vladveterok <vl.berezhnoi@gmail.com>
2020-05-29 23:44:47 +0300

    Add save_to_yaml and load_yaml methods to the Library class

--- library.rb
+++ library.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
+require 'yaml'
+
 # Library class
 class Library
-  # Some code here
+  def initialize
+    # Some code here
+  end
+
+  def save_to_yaml(object, file)
+    File.open(file, 'a') do |f|
+      f.write(object.to_yaml)
+    end
+  end
+
+  def load_yaml(file)
+    File.open(file, 'r') do |f|
+      YAML.load_stream(f)
+    end
+  end
 end
